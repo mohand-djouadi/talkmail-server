5352f11348df44cbbf6840ca0397167ffebafd9c
itshakim213 <hikhlef4@gmail.com>
2024-02-02 13:23:18 +0100

    try to fix sending attach

--- server/config/multerConfig.js
+++ server/config/multerConfig.js
@@ -1,32 +1,17 @@
 const multer = require('multer');
 const path = require('path');
 
-// const upload = multer({
-//   storage: multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       // cb(null, `server/controllers/uploads`); // Dossier où les pièces jointes seront stockées
-//       // cb(null, path.join(__dirname, 'uploads'));
-//       cb(null, path.join(__dirname, 'uploads'));
-//       // cb(null, '/opt/render/project/src/server/config/uploads');
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, `${Date.now()}_${file.originalname}`); // Nom du fichier
-//     },
-//   }),
-// });
-
 const upload = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, 'config', 'uploads'));
+      // cb(null, `server/controllers/uploads`); // Dossier où les pièces jointes seront stockées
+      // cb(null, path.join(__dirname, 'uploads'));
+      cb(null, '/opt/render/project/src/server/config/uploads');
     },
     filename: function (req, file, cb) {
-      cb(null, `${Date.now()}_${file.originalname}`);
+      cb(null, `${Date.now()}_${file.originalname}`); // Nom du fichier
     },
   }),
 });
 
 module.exports = upload;
-
-
-
