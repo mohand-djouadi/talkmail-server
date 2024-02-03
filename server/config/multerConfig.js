87a6a5d868792aed02fe73cce06b339eec5c4615
itshakim213 <hikhlef4@gmail.com>
2024-02-03 14:53:18 +0100

    path send attach2

--- server/config/multerConfig.js
+++ server/config/multerConfig.js
@@ -5,8 +5,8 @@ const upload = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
       // cb(null, `/opt/render/project/src/server/config/uploads/`); // Dossier où les pièces jointes seront stockées
-      // cb(null, path.join(__dirname, 'uploads'));
-      const filePath = path.join(__dirname, 'uploads', filename);
+      cb(null, path.join(__dirname, 'uploads'));
+      // const filePath = path.join(__dirname, 'uploads', filename);
     },
 
     filename: function (req, file, cb) {
